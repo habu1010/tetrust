bbb2141827b599050597d964fdf1e057c6c5c4a7
Habu <habu1010+github@gmail.com>
2023-02-08 22:44:31 +0900

    feat: 出現するテトリミノの種類をランダムにする
    I-テトリミノだけでなく、他の6種のテトリミノもランダムに出現するようにする。

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use getch_rs::{Getch, Key};
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
 
@@ -22,6 +26,20 @@ enum MinoKind {
     J,
     L,
     T,
+}
+
+impl Distribution<MinoKind> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> MinoKind {
+        match rng.gen_range(0..=6) {
+            0 => MinoKind::I,
+            1 => MinoKind::O,
+            2 => MinoKind::S,
+            3 => MinoKind::Z,
+            4 => MinoKind::J,
+            5 => MinoKind::L,
+            _ => MinoKind::T,
+        }
+    }
 }
 
 // テトリミノの形状
@@ -88,11 +106,11 @@ fn is_collision(field: &FieldSize, pos: &Position, kind: MinoKind) -> bool {
     false
 }
 
-fn draw(field: &FieldSize, pos: &Position) {
+fn draw(field: &FieldSize, pos: &Position, kind: MinoKind) {
     let mut field_buf = field.clone();
     for y in 0..4 {
         for x in 0..4 {
-            field_buf[y + pos.y][x + pos.x] |= MINOS[MinoKind::I as usize][y][x];
+            field_buf[y + pos.y][x + pos.x] |= MINOS[kind as usize][y][x];
         }
     }
 
@@ -136,34 +154,42 @@ fn main() {
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ]));
     let pos = Arc::new(Mutex::new(Position { x: 4, y: 0 }));
+    let mino = Arc::new(Mutex::new(rand::random::<MinoKind>()));
 
     // 画面クリア・カーソル非表示
     println!("\x1b[2J\x1b[H\x1b[?25l");
 
-    draw(&field.lock().unwrap(), &pos.lock().unwrap());
+    draw(
+        &field.lock().unwrap(),
+        &pos.lock().unwrap(),
+        *mino.lock().unwrap(),
+    );
 
     {
         let pos = Arc::clone(&pos);
         let field = Arc::clone(&field);
+        let mino = Arc::clone(&mino);
         let _ = thread::spawn(move || loop {
             thread::sleep(time::Duration::from_millis(1000));
             let mut pos = pos.lock().unwrap();
             let mut field = field.lock().unwrap();
+            let mut mino = mino.lock().unwrap();
             let new_pos = Position {
                 x: pos.x,
                 y: pos.y + 1,
             };
-            if !is_collision(&field, &new_pos, MinoKind::I) {
+            if !is_collision(&field, &new_pos, *mino) {
                 *pos = new_pos;
             } else {
                 for y in 0..4 {
                     for x in 0..4 {
-                        field[y + pos.y][x + pos.x] |= MINOS[MinoKind::I as usize][y][x];
+                        field[y + pos.y][x + pos.x] |= MINOS[*mino as usize][y][x];
                     }
                 }
                 *pos = Position { x: 4, y: 0 };
+                *mino = rand::random();
             }
-            draw(&field, &pos);
+            draw(&field, &pos, *mino);
         });
     }
 
@@ -173,38 +199,41 @@ fn main() {
             Ok(Key::Down) => {
                 let mut pos = pos.lock().unwrap();
                 let field = field.lock().unwrap();
+                let mino = mino.lock().unwrap();
                 let new_pos = Position {
                     x: pos.x,
                     y: pos.y + 1,
                 };
-                if !is_collision(&field, &new_pos, MinoKind::I) {
+                if !is_collision(&field, &new_pos, *mino) {
                     *pos = new_pos;
                 }
-                draw(&field, &pos);
+                draw(&field, &pos, *mino);
             }
             Ok(Key::Left) => {
                 let mut pos = pos.lock().unwrap();
                 let field = field.lock().unwrap();
+                let mino = mino.lock().unwrap();
                 let new_pos = Position {
                     x: pos.x - 1,
                     y: pos.y,
                 };
-                if !is_collision(&field, &new_pos, MinoKind::I) {
+                if !is_collision(&field, &new_pos, *mino) {
                     *pos = new_pos;
                 }
-                draw(&field, &pos);
+                draw(&field, &pos, *mino);
             }
             Ok(Key::Right) => {
                 let mut pos = pos.lock().unwrap();
                 let field = field.lock().unwrap();
+                let mino = mino.lock().unwrap();
                 let new_pos = Position {
                     x: pos.x + 1,
                     y: pos.y,
                 };
-                if !is_collision(&field, &new_pos, MinoKind::I) {
+                if !is_collision(&field, &new_pos, *mino) {
                     *pos = new_pos;
                 }
-                draw(&field, &pos);
+                draw(&field, &pos, *mino);
             }
             Ok(Key::Char('q')) => {
                 // カーソルを表示
