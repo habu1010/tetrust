572f9e064bcddec1a0e99fe776c3ddd96aff3ff9
Habu <habu1010+github@gmail.com>
2023-02-08 23:28:05 +0900

    refactor: テトリミノに関する定義をモジュールにする

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
+mod mino;
+
 use getch_rs::{Getch, Key};
-use rand::{
-    distributions::{Distribution, Standard},
-    Rng,
-};
+use mino::{MinoKind, MINOS};
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
 
@@ -15,85 +14,6 @@ struct Position {
     x: usize,
     y: usize,
 }
-
-// テトリミノの種類
-#[derive(Clone, Copy)]
-enum MinoKind {
-    I,
-    O,
-    S,
-    Z,
-    J,
-    L,
-    T,
-}
-
-impl Distribution<MinoKind> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> MinoKind {
-        match rng.gen_range(0..=6) {
-            0 => MinoKind::I,
-            1 => MinoKind::O,
-            2 => MinoKind::S,
-            3 => MinoKind::Z,
-            4 => MinoKind::J,
-            5 => MinoKind::L,
-            _ => MinoKind::T,
-        }
-    }
-}
-
-// テトリミノの形状
-const MINOS: [[[usize; 4]; 4]; 7] = [
-    [
-        // I
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-    ],
-    [
-        // O
-        [0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0],
-    ],
-    [
-        // S
-        [0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [1, 1, 0, 0],
-        [0, 0, 0, 0],
-    ],
-    [
-        // Z
-        [0, 0, 0, 0],
-        [1, 1, 0, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0],
-    ],
-    [
-        // J
-        [0, 0, 0, 0],
-        [1, 0, 0, 0],
-        [1, 1, 1, 0],
-        [0, 0, 0, 0],
-    ],
-    [
-        // L
-        [0, 0, 0, 0],
-        [0, 0, 1, 0],
-        [1, 1, 1, 0],
-        [0, 0, 0, 0],
-    ],
-    [
-        // T
-        [0, 0, 0, 0],
-        [0, 1, 0, 0],
-        [1, 1, 1, 0],
-        [0, 0, 0, 0],
-    ],
-];
 
 fn is_collision(field: &FieldSize, pos: &Position, kind: MinoKind) -> bool {
     for y in 0..4 {
---
+++ src/mino.rs
@@ -0,0 +1,83 @@
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
+
+// テトリミノの種類
+#[derive(Clone, Copy)]
+pub enum MinoKind {
+    I,
+    O,
+    S,
+    Z,
+    J,
+    L,
+    T,
+}
+
+impl Distribution<MinoKind> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> MinoKind {
+        match rng.gen_range(0..=6) {
+            0 => MinoKind::I,
+            1 => MinoKind::O,
+            2 => MinoKind::S,
+            3 => MinoKind::Z,
+            4 => MinoKind::J,
+            5 => MinoKind::L,
+            _ => MinoKind::T,
+        }
+    }
+}
+
+// テトリミノの形状
+pub const MINOS: [[[usize; 4]; 4]; 7] = [
+    [
+        // I
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+    ],
+    [
+        // O
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+    ],
+    [
+        // S
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [1, 1, 0, 0],
+        [0, 0, 0, 0],
+    ],
+    [
+        // Z
+        [0, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+    ],
+    [
+        // J
+        [0, 0, 0, 0],
+        [1, 0, 0, 0],
+        [1, 1, 1, 0],
+        [0, 0, 0, 0],
+    ],
+    [
+        // L
+        [0, 0, 0, 0],
+        [0, 0, 1, 0],
+        [1, 1, 1, 0],
+        [0, 0, 0, 0],
+    ],
+    [
+        // T
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [1, 1, 1, 0],
+        [0, 0, 0, 0],
+    ],
+];
