50b1a46a68f6f45450df43351f41acc5dd632a85
Habu <habu1010+github@gmail.com>
2023-02-08 23:56:04 +0900

    refactor: ゲームの状態の変数を構造体にまとめてモジュール化

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
+mod game;
 mod mino;
 
+use game::{FieldSize, Game, Position, FIELD_HEIGHT, FIELD_WIDTH};
 use getch_rs::{Getch, Key};
 use mino::{MinoKind, MINOS};
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
-
-const FIELD_WIDTH: usize = 12;
-const FIELD_HEIGHT: usize = 22;
-
-type FieldSize = [[usize; FIELD_WIDTH]; FIELD_HEIGHT];
-
-struct Position {
-    x: usize,
-    y: usize,
-}
 
 fn is_collision(field: &FieldSize, pos: &Position, kind: MinoKind) -> bool {
     for y in 0..4 {
@@ -29,11 +21,11 @@ fn is_collision(field: &FieldSize, pos: &Position, kind: MinoKind) -> bool {
     false
 }
 
-fn draw(field: &FieldSize, pos: &Position, kind: MinoKind) {
+fn draw(Game { field, pos, kind }: &Game) {
     let mut field_buf = field.clone();
     for y in 0..4 {
         for x in 0..4 {
-            field_buf[y + pos.y][x + pos.x] |= MINOS[kind as usize][y][x];
+            field_buf[y + pos.y][x + pos.x] |= MINOS[*kind as usize][y][x];
         }
     }
 
@@ -52,81 +44,50 @@ fn draw(field: &FieldSize, pos: &Position, kind: MinoKind) {
 }
 
 fn main() {
-    let field = Arc::new(Mutex::new([
-        [1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ]));
-    let pos = Arc::new(Mutex::new(Position { x: 4, y: 0 }));
-    let mino = Arc::new(Mutex::new(rand::random::<MinoKind>()));
+    let game = Arc::new(Mutex::new(Game::new()));
 
     // 画面クリア・カーソル非表示
     println!("\x1b[2J\x1b[H\x1b[?25l");
 
-    draw(
-        &field.lock().unwrap(),
-        &pos.lock().unwrap(),
-        *mino.lock().unwrap(),
-    );
+    draw(&game.lock().unwrap());
 
     {
-        let pos = Arc::clone(&pos);
-        let field = Arc::clone(&field);
-        let mino = Arc::clone(&mino);
+        let game = Arc::clone(&game);
         let _ = thread::spawn(move || loop {
             thread::sleep(time::Duration::from_millis(1000));
-            let mut pos = pos.lock().unwrap();
-            let mut field = field.lock().unwrap();
-            let mut mino = mino.lock().unwrap();
+            let mut game = game.lock().unwrap();
             let new_pos = Position {
-                x: pos.x,
-                y: pos.y + 1,
+                x: game.pos.x,
+                y: game.pos.y + 1,
             };
-            if !is_collision(&field, &new_pos, *mino) {
-                *pos = new_pos;
+            if !is_collision(&game.field, &new_pos, game.kind) {
+                game.pos = new_pos;
             } else {
+                let gx = game.pos.x;
+                let gy = game.pos.y;
                 for y in 0..4 {
                     for x in 0..4 {
-                        field[y + pos.y][x + pos.x] |= MINOS[*mino as usize][y][x];
+                        game.field[y + gy][x + gx] |= MINOS[game.kind as usize][y][x];
                     }
                 }
                 for y in 1..FIELD_HEIGHT - 1 {
                     let mut can_erase = true;
                     for x in 0..FIELD_WIDTH {
-                        if field[y][x] == 0 {
+                        if game.field[y][x] == 0 {
                             can_erase = false;
                             break;
                         }
                     }
                     if can_erase {
                         for y2 in (2..=y).rev() {
-                            field[y2] = field[y2 - 1];
+                            game.field[y2] = game.field[y2 - 1];
                         }
                     }
                 }
-                *pos = Position { x: 4, y: 0 };
-                *mino = rand::random();
+                game.pos = Position::init();
+                game.kind = rand::random();
             }
-            draw(&field, &pos, *mino);
+            draw(&game);
         });
     }
 
@@ -134,43 +95,37 @@ fn main() {
     loop {
         match g.getch() {
             Ok(Key::Down) => {
-                let mut pos = pos.lock().unwrap();
-                let field = field.lock().unwrap();
-                let mino = mino.lock().unwrap();
+                let mut game = game.lock().unwrap();
                 let new_pos = Position {
-                    x: pos.x,
-                    y: pos.y + 1,
+                    x: game.pos.x,
+                    y: game.pos.y + 1,
                 };
-                if !is_collision(&field, &new_pos, *mino) {
-                    *pos = new_pos;
+                if !is_collision(&game.field, &new_pos, game.kind) {
+                    game.pos = new_pos;
                 }
-                draw(&field, &pos, *mino);
+                draw(&game);
             }
             Ok(Key::Left) => {
-                let mut pos = pos.lock().unwrap();
-                let field = field.lock().unwrap();
-                let mino = mino.lock().unwrap();
+                let mut game = game.lock().unwrap();
                 let new_pos = Position {
-                    x: pos.x.checked_sub(1).unwrap_or_else(|| pos.x),
-                    y: pos.y,
+                    x: game.pos.x.checked_sub(1).unwrap_or_else(|| game.pos.x),
+                    y: game.pos.y,
                 };
-                if !is_collision(&field, &new_pos, *mino) {
-                    *pos = new_pos;
+                if !is_collision(&game.field, &new_pos, game.kind) {
+                    game.pos = new_pos;
                 }
-                draw(&field, &pos, *mino);
+                draw(&game);
             }
             Ok(Key::Right) => {
-                let mut pos = pos.lock().unwrap();
-                let field = field.lock().unwrap();
-                let mino = mino.lock().unwrap();
+                let mut game = game.lock().unwrap();
                 let new_pos = Position {
-                    x: pos.x + 1,
-                    y: pos.y,
+                    x: game.pos.x + 1,
+                    y: game.pos.y,
                 };
-                if !is_collision(&field, &new_pos, *mino) {
-                    *pos = new_pos;
+                if !is_collision(&game.field, &new_pos, game.kind) {
+                    game.pos = new_pos;
                 }
-                draw(&field, &pos, *mino);
+                draw(&game);
             }
             Ok(Key::Char('q')) => {
                 // カーソルを表示
