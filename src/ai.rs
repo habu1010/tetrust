1ee0108d5d729d0725c2b6957c3d85ca966ad8c6
Habu <habu1010+github@gmail.com>
2023-02-11 06:38:46 +0900

    feat: 自動プレイAI実装(2)
    積まれているブロックの最大の高さと消えるライン数の評価を正規化し、重み付けを行う。

--- src/ai.rs
+++ src/ai.rs
@@ -2,7 +2,7 @@ use crate::block::block_kind;
 use crate::game::*;
 
 pub fn eval(game: &Game) -> Game {
-    let mut elite = (game.clone(), 0, FIELD_HEIGHT);
+    let mut elite = (game.clone(), 0f64);
 
     for rotate_count in 0..=3 {
         let mut game = game.clone();
@@ -24,16 +24,26 @@ pub fn eval(game: &Game) -> Game {
             let line_count = erase_line_count(&game.field);
             let height_max = field_height_max(&game.field);
 
-            if line_count >= elite.1 && height_max <= elite.2 {
+            // 正規化 & 重み付け
+            let line_count = normalization(line_count as f64, 0.0, 4.0);
+            let height_max = 1.0 - normalization(height_max as f64, 0.0, 20.0);
+            let line_count = line_count * 100.0;
+            let height_max = height_max * 1.0;
+
+            let score = line_count + height_max;
+            if elite.1 < score {
                 let mut game = game_rotated.clone();
                 move_block(&mut game, new_pos);
                 elite.0 = game;
-                elite.1 = line_count;
-                elite.2 = height_max;
+                elite.1 = score;
             }
         }
     }
     elite.0
+}
+
+pub fn normalization(value: f64, min: f64, max: f64) -> f64 {
+    (value - min) / (max - min)
 }
 
 pub fn erase_line_count(field: &FieldSize) -> usize {
