d18ce29d20d5588f1eccb0d05cb0f69c443e3236
Habu <habu1010+github@gmail.com>
2023-02-22 05:33:45 +0900

    feat: 自動プレイAI実装(4)
    ホールドを実行した時としない時のそれぞれについての評価を行う。

--- src/ai.rs
+++ src/ai.rs
@@ -4,44 +4,50 @@ use crate::game::*;
 pub fn eval(game: &Game) -> Game {
     let mut elite = (game.clone(), 0f64);
 
-    for rotate_count in 0..=3 {
+    for do_hold in [true, false] {
         let mut game = game.clone();
-        for _ in 0..=rotate_count {
-            rotate_right(&mut game);
+        if do_hold {
+            hold(&mut game);
         }
-        let game_rotated = &game;
+        for rotate_count in 0..=3 {
+            let mut game = game.clone();
+            for _ in 0..=rotate_count {
+                rotate_right(&mut game);
+            }
+            let game_rotated = &game;
 
-        for dx in -4..=5 {
-            let mut game = game_rotated.clone();
-            let new_pos = Position {
-                x: game.pos.x.checked_add_signed(dx).unwrap_or(0),
-                y: game.pos.y,
-            };
-            move_block(&mut game, new_pos);
-            hard_drop(&mut game);
-            fix_block(&mut game);
-
-            let line_count = erase_line_count(&game.field);
-            let height_max = field_height_max(&game.field);
-            let height_diff = diff_in_height(&game.field);
-            let dead_space_count = dead_space_count(&game.field);
-
-            // 正規化 & 重み付け
-            let line_count = normalization(line_count as f64, 0.0, 4.0);
-            let height_max = 1.0 - normalization(height_max as f64, 0.0, 20.0);
-            let height_diff = 1.0 - normalization(height_diff as f64, 0.0, 200.0);
-            let dead_space_count = 1.0 - normalization(dead_space_count as f64, 0.0, 200.0);
-            let line_count = line_count * 100.0;
-            let height_max = height_max * 1.0;
-            let height_diff = height_diff * 10.0;
-            let dead_space_count = dead_space_count * 30.0;
-
-            let score = line_count + height_max + height_diff + dead_space_count;
-            if elite.1 < score {
+            for dx in -4..=5 {
                 let mut game = game_rotated.clone();
+                let new_pos = Position {
+                    x: game.pos.x.checked_add_signed(dx).unwrap_or(0),
+                    y: game.pos.y,
+                };
                 move_block(&mut game, new_pos);
-                elite.0 = game;
-                elite.1 = score;
+                hard_drop(&mut game);
+                fix_block(&mut game);
+
+                let line_count = erase_line_count(&game.field);
+                let height_max = field_height_max(&game.field);
+                let height_diff = diff_in_height(&game.field);
+                let dead_space_count = dead_space_count(&game.field);
+
+                // 正規化 & 重み付け
+                let line_count = normalization(line_count as f64, 0.0, 4.0);
+                let height_max = 1.0 - normalization(height_max as f64, 0.0, 20.0);
+                let height_diff = 1.0 - normalization(height_diff as f64, 0.0, 200.0);
+                let dead_space_count = 1.0 - normalization(dead_space_count as f64, 0.0, 200.0);
+                let line_count = line_count * 100.0;
+                let height_max = height_max * 1.0;
+                let height_diff = height_diff * 10.0;
+                let dead_space_count = dead_space_count * 30.0;
+
+                let score = line_count + height_max + height_diff + dead_space_count;
+                if elite.1 < score {
+                    let mut game = game_rotated.clone();
+                    move_block(&mut game, new_pos);
+                    elite.0 = game;
+                    elite.1 = score;
+                }
             }
         }
     }
